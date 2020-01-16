bf90ec07aec51de9b23a77df684483f860ad9d77
heyheman11 <daniel.zambetto@gmail.com>
2020-01-16 22:19:08 +1100

    Clean up

--- src/AlbumPanel.jsx
+++ src/AlbumPanel.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { HTTP } from "./utils/constants";
 import "./AlbumPanel.scss";
-// import Play from "../play.svg";
 
 const ORIENTATION_VALUES = ["vertical", "horizontal"];
 
